5d73eaa16aa51252bbdcba273ac7a6affead7251
VGrafVirtuozzo <119930720+VGrafVirtuozzo@users.noreply.github.com>
2024-09-02 10:09:23 +0200

    Update HelloController.java

--- src/main/java/com/mkyong/HelloController.java
+++ src/main/java/com/mkyong/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     String hello() {
-        return "Hello24aaaasdas888888asdacbvnnaa111112!";
+        return "Hello24aaaasdassdfsdfsdfsdf888888asdacbvnnaa111112!";
     }
 
 }
